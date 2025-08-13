e305f07a1fd068b1956fdd2544df9081543075be
bellah-git <bellahworld11@gmail.com>
2025-08-13 11:42:32 +0100

    Delete commands/freeze.js

--- commands/freeze.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'freeze',
-  async execute(msg) {
-    await msg.reply("🥶 This chat is now *frozen*! Nobody move...");
-    setTimeout(() => {
-      msg.reply("🔥 Chat *unfrozen*! Carry on.");
-    }, 10000);
-  }
-};
